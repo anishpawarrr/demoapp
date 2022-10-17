471decc859da6d477e9feb736b5c2f8a21b0b700
Anish Pawar <anishpurupawar@gmail.com>
2022-10-18 00:42:24 +0530

    1st commit

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+import  time as t
+
+f = option_menu("title",["one","two",'three'],icons=["house","bi bi-box","bi bi-chat-quote-fill"],orientation="horizontal")
+if(f=="one"):
+    st.write("one")
+st.title("My app")
+st.header("Test app")
+st.caption("This is caption")
+st.text_input("ENA")
+if st.button("CLC"):
+    with st.spinner("Loading"):
+        for i in range(20):
+            t.sleep(0.1)
+    st.success("Done")
+st.write("asfaf")

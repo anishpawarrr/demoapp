fdaae8dce6e9f75955e112a7abe4dd3eaef3a4a4
anishpawarrr <anishpurupawar@gmail.com>
2022-10-21 21:19:55 +0530

    ser

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
-import  time as t
+import time as t
 
-f = option_menu("title",["one","two",'three'],icons=["house","bi bi-box","bi bi-chat-quote-fill"],orientation="horizontal")
+st.set_page_config("set_page_config")
+st.title("My app")
+f = option_menu("Select",["one","two",'three'],icons=["house","bi bi-box","bi bi-chat-quote-fill"],orientation="horizontal")
 if(f=="one"):
     st.write("one")
-st.title("My app")
+
 st.header("Test app")
 st.caption("This is caption")
 st.text_input("ENA")
